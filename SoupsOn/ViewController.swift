b47856aa4080450ec26d6d6b48bc3a4708d21ed5
chrisyandev <czyanhome@hotmail.com>
2022-03-11 17:53:46 -0800

    Added launch screen and Login/Signup screen

--- SoupsOn/ViewController.swift
+++ SoupsOn/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var signupBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loginBtn.layer.shadowColor = UIColor.gray.cgColor
+        loginBtn.layer.shadowOpacity = 1
+        loginBtn.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        loginBtn.layer.shadowRadius = 5.0
+        
+        signupBtn.layer.shadowColor = UIColor.gray.cgColor
+        signupBtn.layer.shadowOpacity = 1
+        signupBtn.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        signupBtn.layer.shadowRadius = 5.0
+        
     }
-
 
 }
 
