65f1a6738b242227fa52bdbc97ea73c05b1414ed
chrisyandev <czyanhome@hotmail.com>
2022-04-02 01:00:10 -0700

    Correctly save strings from reusable cell text fields

--- SoupsOn/UserCreateRecipeViewController.swift
+++ SoupsOn/UserCreateRecipeViewController.swift
@@ -1,16 +1,21 @@
 import UIKit
+import Firebase
 
 class UserCreateRecipeViewController: UIViewController {
     
     var titleValue: String = "Create Recipe"
+    let db = Firestore.firestore()
     var numOfIngredients: Int = 1
     var numOfDirections: Int = 1
+    var ingredientsData: [String] = [""]
+    var directionsData: [String] = [""]
     
     @IBOutlet weak var recipeNameTF: UITextField!
     @IBOutlet weak var servingsTF: UITextField!
     @IBOutlet weak var timeToMakeTF: UITextField!
     @IBOutlet weak var ingredientsTV: UITableView!
     @IBOutlet weak var directionsTV: UITableView!
+    @IBOutlet weak var errorMsg: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +28,43 @@ class UserCreateRecipeViewController: UIViewController {
     }
     
     @IBAction func saveRecipeBtnPressed(_ sender: Any) {
+        let recipeNameIsEmpty = recipeNameTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        let servingsIsEmpty = servingsTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        let timeToMakeIsEmpty = timeToMakeTF.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        if recipeNameIsEmpty || servingsIsEmpty || timeToMakeIsEmpty {
+            errorMsg.text = "Fields cannot be empty"
+        }
         
+    }
+    
+    @objc func ingredientTextFieldDidChange(_ textField: UITextField) {
+        if let text = textField.text {
+            ingredientsData[textField.tag] = text
+        } else {
+            ingredientsData[textField.tag] = ""
+        }
+        print(ingredientsData)
+    }
+    
+    @objc func directionTextFieldDidChange(_ textField: UITextField) {
+        if let text = textField.text {
+            directionsData[textField.tag] = text
+        } else {
+            directionsData[textField.tag] = ""
+        }
+        print(directionsData)
     }
     
     @objc func addIngredientRow() {
         numOfIngredients += 1
+        ingredientsData.append("")
         ingredientsTV.reloadData()
         ingredientsTV.scrollToRow(at: [0, numOfIngredients - 1], at: .bottom, animated: true)
     }
     
     @objc func addDirectionRow() {
         numOfDirections += 1
+        directionsData.append("")
         directionsTV.reloadData()
         directionsTV.scrollToRow(at: [0, numOfDirections - 1], at: .bottom, animated: true)
     }
@@ -54,6 +85,9 @@ extension UserCreateRecipeViewController: UITableViewDelegate, UITableViewDataSo
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if tableView === ingredientsTV {
             let cell = ingredientsTV.dequeueReusableCell(withIdentifier: "ingredientTableCell") as! IngredientTableCell
+            cell.ingredientTF.text = ingredientsData[indexPath.row]
+            cell.ingredientTF.addTarget(self, action: #selector(ingredientTextFieldDidChange(_:)), for: .editingChanged)
+            cell.ingredientTF.tag = indexPath.row
             cell.addRowBtn.addTarget(self, action: #selector(addIngredientRow), for: .touchUpInside)
             if indexPath.row < (numOfIngredients - 1) {
                 cell.addRowBtn.isHidden = true
@@ -63,6 +97,9 @@ extension UserCreateRecipeViewController: UITableViewDelegate, UITableViewDataSo
             return cell
         } else {
             let cell = directionsTV.dequeueReusableCell(withIdentifier: "directionTableCell") as! DirectionTableCell
+            cell.directionTF.text = directionsData[indexPath.row]
+            cell.directionTF.addTarget(self, action: #selector(directionTextFieldDidChange(_:)), for: .editingChanged)
+            cell.directionTF.tag = indexPath.row
             cell.addRowBtn.addTarget(self, action: #selector(addDirectionRow), for: .touchUpInside)
             if indexPath.row < (numOfDirections - 1) {
                 cell.addRowBtn.isHidden = true
